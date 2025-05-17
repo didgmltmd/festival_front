7de0a6139f81f17423f49beb52ea6496ff69d512
didgmltmd <gomboy11@naver.com>
2025-05-17 16:25:40 +0900

    fix:alert 삭제

--- festival_front/src/function/saveToLocal.js
+++ festival_front/src/function/saveToLocal.js
@@ -10,10 +10,9 @@ const saveToLocal = async () => {
       localStorage.setItem('menuBackup', JSON.stringify(menuRes.data));
       localStorage.setItem('ordersBackup', JSON.stringify(ordersRes.data));
 
-      alert('데이터가 localStorage에 저장되었습니다.');
+
     } catch (error) {
       console.error('데이터 저장 실패:', error);
-      alert('데이터 저장에 실패했습니다.');
     }
   };
 
